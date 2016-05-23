fcc273b14795a424168188251644c1aaae1ef0af
Hannes Muehleisen <hannes@muehleisen.org>
2016-05-23 15:56:32 +0200

    dummy constructors

---
+++ R/dbi.R
@@ -0,0 +1,10 @@
+MonetR <- MonetDB <- MonetDBR <- MonetDB.R <- RMonetDB <- function() {
+  .Deprecated("Use MonetDBLite instead")
+  MonetDBLite::MonetDB.R()
+}
+
+MonetDBLite <- monetdblite <- RMonetDBLite <- rmonetdblite <- function() {
+  .Deprecated("Use MonetDBLite instead")
+  MonetDBLite::MonetDBLite()
+}
+
